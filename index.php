0ed052711627fecf936a7da29bee2e30ce3b2ed6
Diony <dionybusker@gmail.com>
2021-06-11 12:17:07 +0200

    Display lists + tasks in cards

--- index.php
+++ index.php
@@ -8,34 +8,49 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <?php foreach ($lists as $list) { ?>
-        <li>
-            <?php echo $list["name"] ?>
-            <ul>
-            <?php
-                foreach ($tasks as $task) {
-                    if ($task["list_id"] == $list["id"]) {
-            ?>
-                <li>
-                    <?php echo $task["name"] ?>
-                </li>
-            <?php
-                    }
-                }
-            ?>
-            </ul>
-        </li>
-        <?php } ?>
-    </ul>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <div class="row col-12">
+            <?php foreach ($lists as $list) { ?>
+
+            <div class="card col-3 mx-1">
+                <div class="card-body">
+                    <h4 class="card-title"><?php echo $list["name"] ?></h4>
+                    <p class="card-text">
+                        <?php
+                            foreach ($tasks as $task) {
+                                if ($task["list_id"] == $list["id"]) {
+                        ?>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <p class="card-text">
+                                                <span class="font-weight-bold">Taak: </span> <?php echo $task["name"]; ?> <br>
+                                                <span class="font-weight-bold">Beschrijving: </span> <?php echo $task["description"] ?> <br>
+                                                <span class="font-weight-bold">Status: </span> <?php echo $task["status_id"] ?> <br>
+                                            </p>
+                                        </div>
+                                    </div>
+                        <?php
+                                }
+                            }
+                        ?>
+                    </p>
+                </div>
+            </div>
+
+            <?php } ?>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+    </body>
 </html>
