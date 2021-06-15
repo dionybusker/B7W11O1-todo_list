8ec3b0b98bbaa08eb95975a2247f0c4e3aa62458
Diony <dionybusker@gmail.com>
2021-06-15 19:59:15 +0200

    Header and footer in another file

--- createTask.php
+++ createTask.php
@@ -2,6 +2,8 @@
     require_once("includes/functions.php");
     $lists = getAllLists();
     $statuses = getAllStatuses();
+
+    $id = $_GET["id"];
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // $taskName = $_POST["taskName"];
@@ -15,17 +17,10 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Document</title>
-    </head>
-    <body>
-        
+<?php require_once("includes/header.php") ?>
+
+
+        <?php echo $id ?>
         <form class="form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <div class="form-group">
                 <label class="col-1" for="taskName">Taak naam: </label>
@@ -46,7 +41,9 @@
                 <label class="col-1" for="list">Selecteer lijst</label>
                 <select class="col-2" name="list" id="list">
                     <?php foreach ($lists as $list) { ?>
-                        <option value="<?php echo $list["id"] ?>"><?php echo $list["name"] ?></option>
+                        <option value="<?php echo $list["id"] ?>" <?php if ($list["id"] == $id) { echo "selected"; } ?>>
+                            <?php echo $list["name"] ?>
+                        </option>
                     <?php } ?>
                 </select>
             </div>
@@ -63,9 +60,4 @@
             <input type="submit" class="btn btn-info">
         </form>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-            
-    </body>
-</html>
+<?php require_once("includes/footer.php") ?>
