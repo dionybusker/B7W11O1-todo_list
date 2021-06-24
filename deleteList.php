226f8318e18b91099a74eb02d599c9aff412d022
Diony <dionybusker@gmail.com>
2021-06-24 22:15:21 +0200

    Changed a button color

--- deleteList.php
+++ deleteList.php
@@ -9,6 +9,7 @@
         deleteList($id);
 
         header("Location: index.php");
+        exit;
     }
 ?>
 
@@ -17,7 +18,7 @@
 
         <p>Weet je zeker dat je de lijst "<?php $list["name"] ?>" wilt verwijderen? Alle taken worden dan ook verwijderd. </p>
 
-        <a href="index.php" class="btn btn-info">Annuleren</a>
+        <a href="index.php" class="btn btn-dark">Annuleren</a>
 
         <form class="form" action="<?php $_SERVER["PHP_SELF"] ?>?id=<?php echo $id ?>" method="POST">
             <input type="text" value="<?php echo $id ?>" hidden>
--- deleteTask.php
+++ deleteTask.php
@@ -9,6 +9,7 @@
         deleteTask($id);
 
         header("Location: index.php");
+        exit;
     }
 ?>
 
@@ -17,7 +18,7 @@
 
         <h4>Weet je zeker dat je "<?php echo $task["name"] ?>" wilt verwijderen?</h4>
 
-        <a href="index.php" class="btn btn-info">Annuleren</a>
+        <a href="index.php" class="btn btn-dark">Annuleren</a>
 
         <form class="form" action="<?php echo $_SERVER["PHP_SELF"] ?>?id=<?php echo $id ?>" method="POST">
             <input type="text" value="<?php echo $id ?>" hidden>
